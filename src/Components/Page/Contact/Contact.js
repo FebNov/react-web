8f8728053195febcef763be6a8e982a4df60201f
Hunterprey <595129026@qq.com>
2020-09-03 19:10:35 +1000

    add react-copy-to-clipboard module for click to copy email& phone

--- src/Components/Page/Contact/Contact.js
+++ src/Components/Page/Contact/Contact.js
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Styles from "./Contact.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const Contact = () => (
-  <div className={Styles.Container}>
-    <div className={Styles.ContactHeading}>
-      <h5>Contact</h5>
-    </div>
-    <ul className={Styles.MediaSource}>
-      <li className={Styles.SourceItem}>
-        <FontAwesomeIcon icon="phone-square-alt" size="2x" />
-      </li>
-      <li className={Styles.SourceItem}>
-        <FontAwesomeIcon icon="envelope" size="2x" />
-      </li>
-      <li className={Styles.SourceItem}>
-        <a
-          href="https://www.linkedin.com/in/febnov-du/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" />
-        </a>
-      </li>
-      <li className={Styles.SourceItem}>
-        <a
-          href="https://github.com/FebNov/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={["fab", "github-square"]} size="2x" />
-        </a>
-      </li>
-    </ul>
-  </div>
-);
+import { CopyToClipboard } from "react-copy-to-clipboard";
+const Contact = () => {
+  const [clickboard, setClickboard] = useState({
+    copied: false,
+  });
 
+  return (
+    <div className={Styles.Container}>
+      <div className={Styles.ContactHeading}>
+        <h5>Contact</h5>
+      </div>
+      <ul className={Styles.MediaSource}>
+        <li className={Styles.SourceItem}>
+          <FontAwesomeIcon icon="phone-square-alt" size="2x" />
+        </li>
+        <li className={Styles.SourceItem}>
+          <CopyToClipboard
+            text="dushangjie@gmail.com"
+            onCopy={() => {
+              setClickboard({
+                copied: true,
+              });
+            }}
+          >
+            <FontAwesomeIcon icon="envelope" size="2x" />{" "}
+          </CopyToClipboard>
+          {clickboard.copied ? (
+            <div className="ContactToolTip">Email copied</div>
+          ) : null}
+        </li>
+        <li className={Styles.SourceItem}>
+          <a
+            href="https://www.linkedin.com/in/febnov-du/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" />
+          </a>
+        </li>
+        <li className={Styles.SourceItem}>
+          <a
+            href="https://github.com/FebNov/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "github-square"]} size="2x" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 export default Contact;
--- src/Components/Page/Page.js
+++ src/Components/Page/Page.js
@@ -6,65 +6,65 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import Project from "./Project";
 import Resume from "./Resume";
-import ScrollingColorBackground from "react-scrolling-color-background";
-import Styles from "./Page.module.css";
+// import ScrollingColorBackground from "react-scrolling-color-background";
+// import Styles from "./Page.module.css";
 const Page = () => {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
   return (
     <div>
-      <ScrollingColorBackground
+      {/* <ScrollingColorBackground
         selector=".js-color-stop[data-background-color]"
         colorDataAttribute="data-background-color"
         initialRgb="rgb(0, 0, 0)"
         className={Styles.Fix}
-      />
-
+      /> */}
+      {/* 
       <section
         data-background-color="rgb(32, 202, 172)"
         className="js-color-stop"
         id="home"
-      >
-        <Home />
-      </section>
+      > */}
+      <Home />
+      {/* </section> */}
 
-      <section
+      {/* <section
         data-background-color="rgb(60, 191, 246)"
         className="js-color-stop"
         id="about"
-      >
-        <div data-aos="fade-up">
-          <About />
-        </div>
-      </section>
-      <section
+      > */}
+      <div data-aos="fade-up">
+        <About />
+      </div>
+      {/* </section> */}
+      {/* <section
         data-background-color="rgb(60, 191, 246)"
         className="js-color-stop"
         id="project"
-      >
-        <div data-aos="fade-left">
-          <Project />
-        </div>
-      </section>
-      <section
+      > */}
+      <div data-aos="fade-left">
+        <Project />
+      </div>
+      {/* </section> */}
+      {/* <section
         data-background-color="rgb(60, 191, 246)"
         className="js-color-stop"
         id="contact"
-      >
-        <div data-aos="fade-right">
-          <Contact />
-        </div>
-      </section>
-      <section
+      > */}
+      <div data-aos="fade-right">
+        <Contact />
+      </div>
+      {/* </section> */}
+      {/* <section
         data-background-color="rgb(60, 191, 246)"
         className="js-color-stop"
         id="resume"
-      >
-        <div data-aos="fade-up">
-          <Resume />
-        </div>
-      </section>
+      > */}
+      <div data-aos="fade-up">
+        <Resume />
+      </div>
+      {/* </section> */}
     </div>
   );
 };
